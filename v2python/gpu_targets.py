19987318be1ee65843288aeffe5bf63d1e9ec75c
Aaryaman Vasishta <jem456.vasishta@gmail.com>
2025-03-27 02:33:51 +0000

    [WIP] gfx1151

--- v2python/gpu_targets.py
+++ v2python/gpu_targets.py
@@ -8,6 +8,7 @@ AOTRITON_SUPPORTED_GPUS = {
     'Navi32'    : 'GPU_ARCH_AMD_GFX1101',
     'Unidentified'    : 'GPU_ARCH_AMD_GFX950',
     'RX9070XT'  : 'GPU_ARCH_AMD_GFX1201',
+    'StrixHalo'  : 'GPU_ARCH_AMD_GFX1151',
 }
 
 AOTRITON_GPU_ARCH_TUNING_STRING = {
@@ -15,6 +16,7 @@ AOTRITON_GPU_ARCH_TUNING_STRING = {
     'MI300X'    : 'gfx942',
     'Navi31'    : 'gfx1100',
     'Navi32'    : 'gfx1101',
+    'StrixHalo'    : 'gfx1151',
     'Unidentified'    : 'gfx950',
     'RX9070XT'  : 'gfx1201',
 }
@@ -22,6 +24,7 @@ AOTRITON_GPU_ARCH_TUNING_STRING = {
 AOTRITON_TUNING_DATABASE_REUSE = {
     'gfx950' : 'gfx942',
     'gfx1201' : 'gfx1100',
+    'gfx1151' : 'gfx1100',
 }
 
 AOTRITON_GPU_WARPSIZE = {
@@ -29,6 +32,7 @@ AOTRITON_GPU_WARPSIZE = {
     'MI300X'    : 64,
     'Navi31'    : 32,
     'Navi32'    : 32,
+    'StrixHalo'    : 32,
     'Unidentified'    : 64,
     'RX9070XT'  : 32,
 }
